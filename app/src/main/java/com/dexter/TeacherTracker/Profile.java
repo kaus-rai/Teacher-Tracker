3f119ec6c4edb3abfe9fb296e7b9e3ec67a2d7ca
Rohith09 <kandirohith09@gmail.com>
2018-01-28 21:17:07 +0530

    Extension and Minor Changes on Spinner.

--- app/src/main/java/com/dexter/TeacherTracker/Profile.java
+++ app/src/main/java/com/dexter/TeacherTracker/Profile.java
@@ -2,11 +2,13 @@ package com.dexter.TeacherTracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView name;
     Spinner sp;
@@ -19,8 +21,21 @@ public class Profile extends AppCompatActivity {
         String[] status={"Available","Busy",};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
         sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(this);
         //  name=findViewById(R.id.name1);
        // String username=getIntent().getExtras().getString("name");
         //name.setText(username);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
+        String item = (String) parent.getItemAtPosition(pos);
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
