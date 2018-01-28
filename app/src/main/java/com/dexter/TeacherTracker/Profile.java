2aec360df7e2da8aa47a4efcb05f7feafedb74be
Rohith09 <kandirohith09@gmail.com>
2018-01-28 21:01:34 +0530

    Added Spinner for Setting the Status.

--- app/src/main/java/com/dexter/TeacherTracker/Profile.java
+++ app/src/main/java/com/dexter/TeacherTracker/Profile.java
@@ -2,18 +2,25 @@ package com.dexter.TeacherTracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
     TextView name;
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-        name=findViewById(R.id.name1);
-        String username=getIntent().getExtras().getString("name");
-        name.setText(username);
+        sp=findViewById(R.id.spinner);
+        String[] status={"Available","Busy",};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
+        sp.setAdapter(adapter);
+        //  name=findViewById(R.id.name1);
+       // String username=getIntent().getExtras().getString("name");
+        //name.setText(username);
     }
 }
