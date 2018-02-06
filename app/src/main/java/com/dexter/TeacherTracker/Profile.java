457293515270e21dfd393001c2507e11da6ac7ef
kaus2595 <kausr2595@gmail.com>
2018-02-07 03:01:35 +0530

    Logout Button added

--- app/src/main/java/com/dexter/TeacherTracker/Profile.java
+++ app/src/main/java/com/dexter/TeacherTracker/Profile.java
@@ -1,5 +1,6 @@
 package com.dexter.TeacherTracker;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +9,33 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
 
     TextView name;
     Spinner sp;
+    GoogleApiClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         sp=findViewById(R.id.spinner);
         String[] status={"Available","Busy",};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
@@ -32,6 +51,26 @@ public class Profile extends AppCompatActivity implements AdapterView.OnItemSele
         String item = (String) parent.getItemAtPosition(pos);
 
 
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // ...
+            case R.id.sign_out:
+                signOut();
+                break;
+            // ...
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
     }
 
     @Override
