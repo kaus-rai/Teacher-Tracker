718ed7e340ccb2ead1bcc73d4cabb965ed752806
kaus2595 <kausr2595@gmail.com>
2018-03-10 01:49:57 +0530

    Commented out error

--- app/src/main/java/com/dexter/TeacherTracker/Profile.java
+++ app/src/main/java/com/dexter/TeacherTracker/Profile.java
@@ -1,80 +1,80 @@
-package com.dexter.TeacherTracker;
-
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
-
-    TextView name;
-    Spinner sp;
-    GoogleApiClient mGoogleSignInClient;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestProfile()
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        sp=findViewById(R.id.spinner);
-        String[] status={"Available","Busy",};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
-        sp.setAdapter(adapter);
-        sp.setOnItemSelectedListener(this);
-        //  name=findViewById(R.id.name1);
-       // String username=getIntent().getExtras().getString("name");
-        //name.setText(username);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
-        String item = (String) parent.getItemAtPosition(pos);
-
-
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            // ...
-            case R.id.sign_out:
-                signOut();
-                break;
-            // ...
-        }
-    }
-
-    private void signOut() {
-        mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-}
+//package com.dexter.TeacherTracker;
+//
+//import android.support.annotation.NonNull;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.Spinner;
+//import android.widget.TextView;
+//
+//import com.google.android.gms.auth.api.Auth;
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//
+//public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
+//
+//    TextView name;
+//    Spinner sp;
+//    GoogleApiClient mGoogleSignInClient;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.profile);
+//
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestProfile()
+//                .requestEmail()
+//                .build();
+//
+//        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this, this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
+//
+//        sp=findViewById(R.id.spinner);
+//        String[] status={"Available","Busy",};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
+//        sp.setAdapter(adapter);
+//        sp.setOnItemSelectedListener(this);
+//        //  name=findViewById(R.id.name1);
+//       // String username=getIntent().getExtras().getString("name");
+//        //name.setText(username);
+//    }
+//
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
+//        String item = (String) parent.getItemAtPosition(pos);
+//
+//
+//    }
+//
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            // ...
+//            case R.id.sign_out:
+//                signOut();
+//                break;
+//            // ...
+//        }
+//    }
+//
+//    private void signOut() {
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//    }
+//}
