0a9473afa70e471ceb5356072bb5a6e7cc9c6f10
kaus2595 <kausr2595@gmail.com>
2018-04-18 00:35:21 +0530

    Maps File addition

--- app/src/main/java/com/dexter/TeacherTracker/Map.java
+++ app/src/main/java/com/dexter/TeacherTracker/Map.java
@@ -3,11 +3,15 @@ package com.dexter.TeacherTracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import es.situm.sdk.SitumSdk;
+
 public class Map extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SitumSdk.init(this);
         setContentView(R.layout.activity_map);
+
     }
 }
