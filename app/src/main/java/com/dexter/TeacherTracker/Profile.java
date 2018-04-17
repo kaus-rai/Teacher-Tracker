36c580eb47690b94567915d67bd226296d059425
Rohith09 <kandirohith09@gmail.com>
2018-04-17 20:30:31 +0530

    Added Code for location

--- app/src/main/java/com/dexter/TeacherTracker/Profile.java
+++ app/src/main/java/com/dexter/TeacherTracker/Profile.java
@@ -1,11 +1,22 @@
 package com.dexter.TeacherTracker;
 
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -13,19 +24,29 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+import java.util.jar.*;
+
+public class Profile extends AppCompatActivity implements AdapterView.OnItemSelectedListener, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     TextView name;
     Spinner sp;
     GoogleApiClient mGoogleSignInClient;
+    Button sign_out, location;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        sign_out = findViewById(R.id.sign);
+        sign_out.setOnClickListener(this);
+        location = findViewById(R.id.locations);
+        location.setOnClickListener(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestProfile()
@@ -37,14 +58,14 @@ public class Profile extends AppCompatActivity implements AdapterView.OnItemSele
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        sp=findViewById(R.id.spinner);
-        String[] status={"Available","Busy",};
+        sp = findViewById(R.id.spinner);
+        String[] status = {"Available", "Busy",};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, status);
         sp.setAdapter(adapter);
         sp.setOnItemSelectedListener(this);
-         name=findViewById(R.id.name1);
-        String username=getIntent().getExtras().getString("name");
-        name.setText(username);
+        name = findViewById(R.id.name1);
+        //String username = getIntent().getExtras().getString("name");
+       // name.setText(username);
     }
 
     @Override
@@ -56,13 +77,87 @@ public class Profile extends AppCompatActivity implements AdapterView.OnItemSele
 
     public void onClick(View v) {
         switch (v.getId()) {
-            // ...
-            //case R.id.sign_out:
-              //  signOut();
-               // break;
-            // ...
+
+            case R.id.sign_out:
+                signOut();
+                break;
+
+            case R.id.locations:
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+
+                        double lat = location.getLatitude();
+                        double lon = location.getLongitude();
+                        Log.e("Lat", String.valueOf(lat));
+                        Log.e("Lng", String.valueOf(lon));
+                    }
+
+
+                    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                    }
+
+                    public void onProviderEnabled(String s) {
+
+                    }
+
+                    public void onProviderDisabled(String s) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+
+                    }
+
+
+                };
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                } else {
+                    configureButton();
+                }
+
+
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode)
+        {
+            case 10:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                    return;
+
+
+        }
+    }
+
+    private void configureButton() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 300000, 0, (android.location.LocationListener) locationListener);
+    }
+
+
 
     private void signOut() {
      //   mGoogleSignInClient.signOut.addOnCompleteListener(this, new OnCompleteListener<Void>() {
