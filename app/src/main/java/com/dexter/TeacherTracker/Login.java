68abe6631c46e4a0a8541ac5d22605a4b3626a88
[Rachit] <[rachitnagwani13@gmail.com]>
2018-04-20 21:35:09 +0530

    Differentiate between teacher and student

--- app/src/main/java/com/dexter/TeacherTracker/Login.java
+++ app/src/main/java/com/dexter/TeacherTracker/Login.java
@@ -1,5 +1,15 @@
 package com.dexter.TeacherTracker;
+import android.*;
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,20 +24,52 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener {
+import java.util.Map;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener, GoogleApiClient.ConnectionCallbacks, ResultCallback {
     SignInButton signin;
     TextView text;
     String name;
+    String email;
     GoogleApiClient mGoogle;
     private static final String TAG="SignIn Activity";
     private static final int sign=9001;
     TextView status;
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest locationRequest;
+    int REQUEST_CHECK_SETTINGS = 100;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+       mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+        mGoogleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+    
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestProfile()
                 .requestEmail()
@@ -40,6 +82,61 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         signin = (SignInButton) findViewById(R.id.signin);
         signin.setOnClickListener(this);
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        builder.build()
+                );
+
+        result.setResultCallback(this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+        final Status status = result.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+
+                // NO need to show the dialog;
+
+                break;
+
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                //  Location settings are not satisfied. Show the user a dialog
+
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+
+                    status.startResolutionForResult(Login.this, REQUEST_CHECK_SETTINGS);
+
+                } catch (IntentSender.SendIntentException e) {
+
+                    //failed to show
+                }
+                break;
+
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                // Location settings are unavailable so not possible to show any dialog now
+                break;
+        }
+    }
+
+    @Override
+
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.signin:
@@ -51,6 +148,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         Intent signInIntent= Auth.GoogleSignInApi.getSignInIntent(mGoogle);
         startActivityForResult(signInIntent,sign);
     }
+    @Override
     public void onActivityResult(int requestcode, int resultcode, Intent data)
     {
         super.onActivityResult(requestcode,resultcode,data);
@@ -59,27 +157,67 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
-    }
+        if (requestcode == REQUEST_CHECK_SETTINGS) {
+
+            if (resultcode == RESULT_OK) {
+
+                Toast.makeText(getApplicationContext(), "GPS enabled", Toast.LENGTH_LONG).show();
+            } else {
+
+                Toast.makeText(getApplicationContext(), "GPS is not enabled", Toast.LENGTH_LONG).show();
+            }
+
+        }}
     private void handleSignInResult(GoogleSignInResult result)
     {
         Log.d(TAG,"handleSignInResult:"+result.isSuccess());
         if(result.isSuccess())
         {
             GoogleSignInAccount acc=result.getSignInAccount();
-            Intent i=new Intent(getApplicationContext(),branches.class);
             name=acc.getDisplayName();
-//            i.putExtra("name",name);
-            Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
-            startActivity(i);
-            finish();
+            email=acc.getEmail();
+            email   = email.substring(0, email.lastIndexOf("@"));
+            DatabaseReference myref = database.getReference("Student");
+            myref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Map <String, Object> value= (Map<String, Object>) dataSnapshot.getValue();
+                    Boolean b = value.containsKey(email);
+                    if (b)
+                    {
+                        Intent i=new Intent(getApplicationContext(),branches.class);
+                        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+                        startActivity(i);
+                        finish();
+                    }
+                    else
+                    {
+                        Intent i=new Intent(getApplicationContext(),Teachers.class);
+                        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+                        startActivity(i);
+                        finish();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(Login.this, "Error-"+databaseError.toException(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+
         }
         else{
             Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
         }
 
     }
+    @Override
     public void onConnectionFailed(ConnectionResult ConnectionResult){
         Log.d(TAG, "onConnectionFailed:"+ ConnectionResult);
     }
+
 
 }
